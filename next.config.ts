69d537ce20d5931433c5125b35d3ca0eeabe2373
Fazle Rabbi <fazle.rabbi-v@adityabirlacapital.com>
2024-12-08 15:53:44 +0600

    next, main change

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: "/personal-portfolio",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  output: 'export', // Ensures static site export
+  images: {
+    unoptimized: true, // Required if using Next.js < 13.3.0 and you use next/image
+  },
+  trailingSlash: true, // Ensures URLs have a trailing slash (important for static sites)
+  basePath: '', // Add your GitHub Pages repository name here if needed, e.g., '/your-repo-name'
+
 };
 
 export default nextConfig;
