a53b663a155a5d5069ef7b134f7fd921fbb8e9e9
Fazle Rabbi <fazle.rabbi-v@adityabirlacapital.com>
2024-12-08 16:04:24 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/personal-portfolio",
-  output: "export",  // <=== enables static exports
+  output: "export",
+  trailingSlash: true,
   reactStrictMode: true,
 };
 
