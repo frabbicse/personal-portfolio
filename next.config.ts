59ee5811a193ac2a8b64ed59a9e1650e9ce88f34
Fazle Rabbi <fazle.rabbi-v@adityabirlacapital.com>
2024-12-08 14:48:29 +0600

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/2048-in-react",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
